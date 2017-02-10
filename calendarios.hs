443312ab5bd99327d32f6e902c39547df4cd1bc9
Miguel Ibáñez <miguelibaezgarcia@gmail.com>
2017-02-10 21:51:40 +0100

    Update calendarios.hs

--- calendarios.hs
+++ calendarios.hs
@@ -1,18 +1,18 @@
--- Comentario: usar que el 1-1-2007 cayó en lunes
+-- Notes: use that 2017-1-1 was Monday.
 
 
 dSem :: [(String,Int)]
-dSem  =  [("Lunes",0),("Martes",1),("Miercoles",2),("Jueves",3),("Viernes",4),("Sabado",5),("Domingo",6)]
+dSem  =  [("Monday",0),("Tuesday",1),("Wednesday",2),("Thursday",3),("Friday",4),("Saturday",5),("Sunday",6)]
 
 dMes :: [(Int,Int)]
 dMes  =  [(1,31),(2,28),(3,31),(4,30),(5,31),(6,30),(7,31),(8,31),(9,30),(10,31),(11,30),(12,31)]
 
-
+----------------------------------------------------------------
 
 busca :: a -> [(a1,a2)] -> (a -> a1 -> Bool) -> [(a1,a2)]
 busca x xs p  =  [(x',y) | (x',y) <- xs, p x x']
 
-
+----------------------------------------------------------------
 
 diaAno :: (Int,Int,Int) -> Int
 diaAno (d,m,a)  =  d + sum [y | (x,y) <- busca m dMes (>)]
